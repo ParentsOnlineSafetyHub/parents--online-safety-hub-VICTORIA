7ce6d91ceb9dc14497cf07a0edd6ac416f0cabf9
ParentsOnlineSafetyHub <allthewaycarpentry@gmail.com>
2026-03-02 17:11:28 +1100

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
     <a href="v3-age-guide.html">Age Guide</a>
     <a href="v3-house-rules.html">House Rules</a>
     <a href="v3-redflags.html">Red Flags</a>
-    <a href="v3-algorithm-awareness.html">Algorithm Awareness</a>
     <a href="v3-device-controls.html">Device Controls</a>
     <a href="v3-platforms.html">Platforms</a>
     <a href="v3-gaming.html">Gaming</a>
     <a href="v3-socials.html">Social Apps</a>
     <a href="v3-videochat.html">Video & Live</a>
+   <a href="v3-algorithm-awareness.html">Algorithm Awareness</a>
     <a href="v3-mindset.html">Mindset</a>
     <a href="v3-reporting.html">Reporting</a>
     <a href="v3-downloads.html">Downloads</a>
