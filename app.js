cd8bb8f2f36f47c58ead2de985af960fce0033fe
Graeme Ingle <allthewaycarpentry@gmail.com>
2026-03-05 14:04:41 +1100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
   <nav>
     <a href="index.html">Home</a>
     <a href="v3-start.html">Start</a>
-    <a href="v3-why-posh-exists.html">Why POSH Exists</a>
     <a href="v3-age-guide.html">Age Guide</a>
     <a href="v3-house-rules.html">House Rules</a>
     <a href="v3-redflags.html">Red Flags</a>
@@ -13,17 +12,16 @@
     <a href="v3-socials.html">Social Apps</a>
     <a href="v3-videochat.html">Video & Live</a>
     <a href="v3-algorithm-awareness.html">Algorithm Awareness</a>
+    <a href="v3-predators.html">Predators</a>
+    <a href="v3-grooming.html">Grooming</a>
     <a href="v3-mindset.html">Mindset</a>
     <a href="v3-reporting.html">Reporting</a>
     <a href="v3-downloads.html">Downloads</a>
-    <a href="v3-share.html">Share POSH</a>
     <a href="v3-cancel-subscriptions.html">Cancel Subscriptions</a>
     <a href="v3-support.html">Support</a>
   </nav>
   `;
 
   const target = document.getElementById("nav");
-  if (target) {
-    target.innerHTML = navHTML;
-  }
+  if (target) target.innerHTML = navHTML;
 })();
